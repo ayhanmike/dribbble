117a0e3791036543e5adddacf585871c4b1a19d3
Ayhan Gümüşalan <ayhanmike@hotmail.com>
2021-08-02 23:33:33 +0300

    2nd

--- Posters/Negative-Space-Geometric-Poster/poster.js
+++ Posters/Negative-Space-Geometric-Poster/poster.js
@@ -19,20 +19,3 @@ grid.forEach(item => {
     elem.classList.add(...new_class);
   });
 });
-
-
-// grid.forEach(elem => {
-//   setInterval(function () {
-//     let random_color = Math.floor(Math.random() * colors.length);
-//     let color = colors[random_color];
-
-//     let random_direction = Math.floor(Math.random() * directions.length);
-//     let direction = directions[random_direction];
-
-//     let new_class = "grid-item " + color + " " + direction;
-//     new_class = new_class.split(" ");
-
-//     elem.classList.remove(...elem.classList);
-//     elem.classList.add(...new_class);
-//   }, 300);
-// });
