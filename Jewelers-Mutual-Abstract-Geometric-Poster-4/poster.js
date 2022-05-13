ec6674e65446152c92e25c703471b133d24818a1
Ayhan Gümüşalan <ayhanmike@hotmail.com>
2022-05-13 13:27:34 +0300

    Develop (#7)
    * home fix
    
    * fixes
    
    Co-authored-by: Ayhan Gümüşalan <ayhan@mumzworld.com>

--- Jewelers-Mutual-Abstract-Geometric-Poster-4/poster.js
+++ Jewelers-Mutual-Abstract-Geometric-Poster-4/poster.js
@@ -1,24 +1,28 @@
-
 const grids = document.querySelectorAll(".grid-item");
 
-grids.forEach(item => {
-  item.addEventListener("mouseenter", (e) => {
-    let elem = e.target;
-    if (!elem.classList.contains("grid-item")) {
-      elem = elem.closest(".grid-item");
+const flipHandler = function (e) {
+  let elem = e.target;
+  if (!elem.classList.contains("grid-item")) {
+    elem = elem.closest(".grid-item");
+  }
+  if (elem.classList.contains("flip")) {
+    elem.classList.remove("flip");
+    setTimeout(() => {
+      elem.querySelector(".grid-inner > div:last-child").remove();
+    }, 300);
+  } else {
+    let gridIndex = Math.round(Math.random() * (grids.length - 1))
+    let grid = grids[gridIndex]
+      .querySelector(".grid-inner > div")
+      .cloneNode(true);
+
+    if (!(elem.querySelectorAll(".grid-inner > div").length > 1)) {
+      elem.querySelector(".grid-inner").appendChild(grid);
     }
-    if (elem.classList.contains("flip")) {
-      elem.classList.remove("flip");
-      setTimeout(() => {
-        elem.querySelector(".grid-inner > div:last-child").remove();  
-      }, 300);      
-    } else {
-      let grid = grids[Math.round(Math.random() * (grids.length - 1))].querySelector(".grid-inner > div").cloneNode(true);
-  
-      if (!(elem.querySelectorAll(".grid-inner > div").length > 1)) {
-        elem.querySelector(".grid-inner").appendChild(grid);
-      }
-      elem.classList.add("flip");
-    }    
-  });
+    elem.classList.add("flip");
+  }
+};
+
+grids.forEach((item) => {
+  item.addEventListener("mouseenter", (e) => {flipHandler(e)});
 });
